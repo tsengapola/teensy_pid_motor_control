f89b7bb3a99625e7e01fdcffacb6ea3e2c38d00c
tsengapola <yan_1whun1@hotmail.com>
2020-12-31 14:15:16 +0800

    new Teensy code

--- teensy_PIDwGyro/teensy_PIDwGyro.ino
+++ teensy_PIDwGyro/teensy_PIDwGyro.ino
@@ -1,16 +1,30 @@
 #define ENCODER_OPTIMIZE_INTERRUPTS
 #include <Encoder.h>
-#include <Wire.h>
-#include "SparkFun_BNO080_Arduino_Library.h"
 
+/*IMU*/
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+/*ROS*/
 #include <ros.h>
 #include <ros/time.h>
-
+#include <tf/tf.h>
 #include <tf/transform_broadcaster.h>
 #include <nav_msgs/Odometry.h>   
 #include <geometry_msgs/Twist.h>
 
-BNO080 myIMU;
+
+struct pose_2d{
+  double x,y,theta;
+  double v,vz;
+} ;
+
+/*Car kinematics*/
+double base_radius = 0.3;
+double wheel_diameter = 0.15;
+double ppr = 100;
 
 Encoder motor1(0, 1);
 Encoder motor2(22, 23);
@@ -54,15 +68,13 @@ double out2 = 0;
 double loop_time = 40;
 double loop_time_sec = loop_time/1000.;
 
-/*
- * Servo
- */
-float servo_ang_mapping = 151./1.570796327; //teensy use 9 bit pwm which is 150 equal 90 degree, where mega is 75=90degree
-float servo_ang_mapping_negative = 145./1.570796327; //teensy use 9 bit pwm which is 150 equal 90 degree, where mega is 75=90degree
-float anawrite = 0;
-float servo_nc = 290; //nc point 145-->75 mapping to 90 deg
-int servo_angle_mapping = 290;
+/*IMU*/
+/* Set the delay between fresh samples */
+uint16_t BNO055_SAMPLERATE_DELAY_MS = 20;
 
+// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
+//                                   id, address
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28);
 
 /*
  * ROS
@@ -73,6 +85,7 @@ char odom[] = "/odom";
 char warn[] = "Alive";
 char cbb[] = "cb";
 
+pose_2d pose_2d_ = {0,0,0,0,0};
 geometry_msgs::TransformStamped t;
 nav_msgs::Odometry odom_msg;
 
@@ -81,15 +94,6 @@ tf::TransformBroadcaster broadcaster;
 void cmdCb( const geometry_msgs::Twist& cmd_msg){
   target_rpm = cmd_msg.linear.x;
   target_rpm2 = cmd_msg.linear.y;
-  if(cmd_msg.angular.z>0){
-    anawrite = cmd_msg.angular.z * servo_ang_mapping + servo_nc;
-    anawrite = min(anawrite,servo_nc+50);
-  }
-  else{
-    anawrite = cmd_msg.angular.z * servo_ang_mapping_negative + servo_nc;   
-    anawrite = max(anawrite,servo_nc-50);
-  }
-  servo_angle_mapping = (int)anawrite;
 
 }
 
@@ -111,19 +115,15 @@ void setup() {
   analogWriteResolution(9);
   analogWriteFrequency(pwmPin, 93750); 
   analogWriteFrequency(pwmPin2, 93750); 
-  analogWriteFrequency(servoPin, 488); 
-  
-  Wire.begin();
 
-  if (myIMU.begin() == false)
+  /* Initialise the sensor */
+  if (!bno.begin())
   {
-    Serial.println("BNO080 not detected at default I2C address. Check your jumpers and the hookup guide. Freezing...");
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while (1);
   }
 
-  Wire.setClock(400000); //Increase I2C data rate to 400kHz
-
-  myIMU.enableGameRotationVector(33); //Send data update every 33ms
   
   nh.initNode();
   broadcaster.init(nh);
@@ -131,6 +131,30 @@ void setup() {
   nh.advertise(odom_pub);
 }
 
+void computeOdom(pose_2d& m_pose_2d, double rpm_r, double rpm_l, double vz){
+  
+  if(abs(vz)<0.03)
+    vz = 0.0;
+    
+  double dt_theta = (m_pose_2d.vz+vz)/2.*loop_time_sec;
+  double v_r = rpm_r/60.0 * 3.1415926535*wheel_diameter;
+  double v_l = rpm_l/60.0 * 3.1415926535*wheel_diameter;
+  double v = (v_r+v_l)/2.;
+
+  double base_x_vel = (m_pose_2d.v+v)/2. * cos( dt_theta );
+  double base_y_vel = (m_pose_2d.v+v)/2. * sin( dt_theta );
+
+  
+  double dt_odom_x = ( base_x_vel * cos(m_pose_2d.theta) - base_y_vel * sin(m_pose_2d.theta) ) * loop_time_sec;
+  double dt_odom_y = ( base_x_vel * sin(m_pose_2d.theta) + base_y_vel * cos(m_pose_2d.theta) ) * loop_time_sec;
+
+  m_pose_2d.x += dt_odom_x;
+  m_pose_2d.y += dt_odom_y;
+  m_pose_2d.theta += dt_theta;
+  m_pose_2d.v = v;
+  m_pose_2d.vz = vz;
+  
+}
 
 void loop() {
   
@@ -142,15 +166,17 @@ void loop() {
     digitalWrite(PinB, LOW);  
     digitalWrite(PinC, LOW);
     digitalWrite(PinD, LOW);  
+    pose_2d_ = {0,0,0,0,0};
     nh.spinOnce();
   }
   //nh.logwarn(warn);
+  unsigned long calc_t = millis();
   long directionTick_1, directionTick_2;
   directionTick_1 = motor1.read();
   directionTick_2 = motor2.read();
 
-  measured_rpm = directionTick_1/11.*60.*1000./loop_time/4.; //4 is mapping of teensy
-  measured_rpm2 = directionTick_2/11.*60.*1000./loop_time/4.;
+  measured_rpm = directionTick_1/ppr*60.*1000./loop_time/4.; //4 is mapping of teensy
+  measured_rpm2 = directionTick_2/ppr*60.*1000./loop_time/4.;
     
   motor1.write(0);
   motor2.write(0);
@@ -210,47 +236,42 @@ void loop() {
   analogWrite(pwmPin2, out2);
 
   /*
-   * Get queternion from BNO080
+   * Get queternion from IMU
    */
   
-  float quatI,quatJ,quatK,quatReal;
-  if (myIMU.dataAvailable() == true)
-  {
-    t.header.frame_id = odom;
-    t.child_frame_id = base_link;
-    t.transform.translation.x = 0.0; 
-    t.transform.translation.y = 0.0; 
-    t.transform.translation.z = 0.0; 
-    t.transform.rotation.x = myIMU.getQuatI();
-    t.transform.rotation.y = myIMU.getQuatJ(); 
-    t.transform.rotation.z = myIMU.getQuatK(); 
-    t.transform.rotation.w = myIMU.getQuatReal();
-    t.header.stamp = nh.now();
+  sensors_event_t angVelocityData;
+  bno.getEvent(&angVelocityData,Adafruit_BNO055::VECTOR_GYROSCOPE);
 
-    odom_msg.header = t.header;
-    odom_msg.child_frame_id = base_link;
-    odom_msg.pose.pose.position.x = 0.0;
-    odom_msg.pose.pose.position.y = 0.0;
-    odom_msg.pose.pose.position.z = 0.0;
-    odom_msg.pose.pose.orientation.x = myIMU.getQuatI();
-    odom_msg.pose.pose.orientation.y = myIMU.getQuatJ(); 
-    odom_msg.pose.pose.orientation.z = myIMU.getQuatK(); 
-    odom_msg.pose.pose.orientation.w = myIMU.getQuatReal();
-    odom_msg.twist.twist.linear.x = 0.0;
-    odom_msg.twist.twist.linear.y = 0.0;
-    odom_msg.twist.twist.angular.z = 0.0;
+  /* put rpm of wheels and gyro by rps*/
+  computeOdom(pose_2d_, measured_rpm, measured_rpm2, angVelocityData.gyro.z*0.017453293); //dps to rps
+
+  t.header.frame_id = odom;
+  t.child_frame_id = base_link;
+  t.transform.translation.x = pose_2d_.x; 
+  t.transform.translation.y = pose_2d_.y; 
+  t.transform.translation.z = 0.0; 
+  t.transform.rotation = tf::createQuaternionFromYaw(pose_2d_.theta);
+  t.header.stamp = nh.now();
+
+  odom_msg.header = t.header;
+  odom_msg.child_frame_id = base_link;
+  odom_msg.pose.pose.position.x = pose_2d_.x; 
+  odom_msg.pose.pose.position.y = pose_2d_.y; 
+  odom_msg.pose.pose.position.z = 0.0;
+  odom_msg.pose.pose.orientation = t.transform.rotation;
+  odom_msg.twist.twist.linear.x = pose_2d_.v;
+  odom_msg.twist.twist.linear.y = 0.0;
+  odom_msg.twist.twist.angular.z = pose_2d_.vz;
     
-  }
-  else{
-    delay(loop_time);
-  }
-  
-  analogWrite(servoPin, servo_angle_mapping);
-  //analogWrite(servoPin, servo_nc+151);
+
   broadcaster.sendTransform(t);
   odom_pub.publish(&odom_msg);
   
   nh.spinOnce();
-  delay(loop_time);
+  double cal_time = abs(millis()-calc_t);
+  if(cal_time<=10)
+    delay(loop_time-cal_time);
+  else
+    delay(loop_time);
   
 }
